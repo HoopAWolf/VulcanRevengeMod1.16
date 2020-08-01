3ac9e7e0d70f91ac20a246d01df0a44fedc86cec
HoopAWolf <hoopawolf394@gmail.com>
2020-08-01 13:25:11 +0800

    Transfering somet stuff over, Path finding improvements and some math func and Animation Helper timeeeeeeee

--- src/main/java/com/hoopawolf/vrm/helper/VRMMathHelper.java
+++ src/main/java/com/hoopawolf/vrm/helper/VRMMathHelper.java
@@ -8,4 +8,25 @@ public class VRMMathHelper
     {
         return (start.add(end.subtract(start).mul(percent, percent, percent)));
     }
+
+    public static Vector3d crossProduct(Vector3d vec_A, Vector3d vec_B)
+    {
+        return new Vector3d(Math.signum((int) (vec_A.getY() * vec_B.getZ() - vec_A.getZ() * vec_B.getY())),
+                Math.signum((int) (vec_A.getZ() * vec_B.getX() - vec_A.getX() * vec_B.getZ())),
+                Math.signum((int) (vec_A.getX() * vec_B.getY() - vec_A.getY() * vec_B.getX())));
+    }
+
+    public static double signum(double d)
+    {
+        if (d > 0F && d <= 0.5F)
+            return 0;
+        else if (d > 0.5F)
+            return 1;
+        else if (d > -0.5F && d < 0F)
+            return 0;
+        else if (d < -0.5F)
+            return -1;
+
+        return 0;
+    }
 }
